8eb9eca261bcb723ef8bfc7e392a40a36ab03098
Rene Rendon <rene.rendon@hotmail.com>
2022-12-04 09:35:49 -0600

    End of section 17.

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.Helpers;
 using API.Interfaces;
 using API.Services;
+using API.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
@@ -22,6 +23,8 @@ namespace API.Extensions
             {
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
+            services.AddSignalR();
+            services.AddSingleton<PresenceTracker>();
 
             return services;
         }
