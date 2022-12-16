0808e26e43fc2bdf18d11fe9f353263577f1affd
Rene Rendon <rene.rendon@hotmail.com>
2022-12-15 18:22:16 -0600

    Section 18 prepare to deploy before PostgreSQL

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -20,6 +20,8 @@ export class AppComponent implements OnInit {
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'));
-    this.accountService.setCurrentUser(user);
+    if (user) {
+      this.accountService.setCurrentUser(user);
+    }
   }
 }
